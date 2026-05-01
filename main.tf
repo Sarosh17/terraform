d7f43b6cb112904b134847912f3df5c1dcd42d73
SAROSH AHMED KHAN <89131155+Sarosh17@users.noreply.github.com>
2026-05-01 20:03:32 +0530

    fixed typos

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
     Name = "Learning Terraforn"
@@ -45,7 +45,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_grouo_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in"{
@@ -55,7 +55,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_grouo_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_everything_out"{
   type        = "egress"
@@ -64,5 +64,5 @@ resource "aws_security_group_rule" "blog_everything_out"{
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_grouo_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
