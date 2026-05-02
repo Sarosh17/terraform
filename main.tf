a9fa0952174265d11c87e8bb725d583d61948efc
SAROSH AHMED KHAN <89131155+Sarosh17@users.noreply.github.com>
2026-05-02 13:23:19 +0530

    autoscaling

--- main.tf
+++ main.tf
@@ -37,17 +37,7 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-  tags = {
-    Name = "Learning Terraforn"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
@@ -126,7 +116,45 @@ resource "aws_lb_target_group_attachment" "blog" {
   target_id        = aws_instance.blog.id
   port             = 80
 }
+
+module "blog_autoscaling" {
+source  = "terraform-aws-modules/autoscaling/aws"
+version = "9.2.0"
+          
+
+name = "blog"
+min_size = 1
+max_size = 2
+
+vpc_zone_identifier = module.blog_vpc.public_subnets
+
+launch_template_name = "blog"
+security_groups = module.blog_sg.security_group_id
+instance_type = var.instance_type
+
+image_id          = data.aws_ami.app_ami.id
+
+traffice_source_attachments = {
+  blog-alb = {
+    traffice_source_identifier = aws_lb_target_group.blog.arn
+  }
+}
+}
+
 /*
+resource "aws_instance" "blog" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+  tags = {
+    Name = "Learning Terraforn"
+  }
+}
+
+
 resource "aws_security_group" "blog" {
   name = "blog"
   tags = {
